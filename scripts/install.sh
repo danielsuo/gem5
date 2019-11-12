6cc058ba121a4fe02ca8475ca0073e8465852c52
Daniel Suo <danielsuo@gmail.com>
2019-11-12 15:42:00 -0500

    Update dependencies
    Change-Id: I2a791811266dc6995aec321eef977e12f596c63c

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 sudo apt install build-essential git m4 scons zlib1g zlib1g-dev libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev python-dev python python-pip automake
-
-pip install six
